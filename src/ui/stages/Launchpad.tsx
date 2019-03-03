b338aeb8e51047ba4ea7e2e9d1e1a64330674088
moonrailgun <moonrailgun@gmail.com>
2019-03-03 16:53:57 +0800

    launchpad

--- src/ui/stages/Launchpad.tsx
+++ src/ui/stages/Launchpad.tsx
@@ -6,8 +6,65 @@ const Root = styled.div`
   height: 100%;
 `;
 
+const Title = styled.div`
+  position: absolute;
+  left: 10%;
+  top: 30%;
+  font-size: 48px;
+`;
+
+const Verison = styled.div`
+  position: absolute;
+  left: 10px;
+  bottom: 10px;
+  font-size: 9px;
+  color: #999;
+`;
+
+const Actions = styled.div`
+  position: absolute;
+  right: 10%;
+  bottom: 20%;
+
+  button {
+    display: block;
+    height: 56px;
+    line-height: 44px;
+    width: 160px;
+    margin-bottom: 10px;
+    background-color: transparent;
+    color: black;
+    font-size: 24px;
+    border: 0;
+    outline: 0;
+    cursor: pointer;
+    padding: 10px;
+    transition: font-size ease-in-out 0.1s;
+
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.2);
+      color: white;
+      font-size: 36px;
+    }
+
+    &:active {
+      background-color: rgba(0, 0, 0, 0.4);
+    }
+  }
+`;
+
 export class Launchpad extends React.Component {
   public render() {
-    return <Root>Launchpad</Root>;
+    return (
+      <Root>
+        <Title>Freedom - 自由</Title>
+        <Verison>v0.0.1</Verison>
+        <Actions>
+          <button>开始</button>
+          <button>继续</button>
+          <button>退出</button>
+        </Actions>
+      </Root>
+    );
   }
 }
